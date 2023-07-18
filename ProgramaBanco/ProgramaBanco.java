93b7258a7ece7a2df9a8beffa81204d7f69cb480
igor-linux <igor_cdm@hotmail.com>
2023-07-18 09:07:56 -0300

    programabanco

--- ProgramaBanco/ProgramaBanco.java
+++ ProgramaBanco/ProgramaBanco.java
@@ -5,7 +5,7 @@ public class ProgramaBanco {
         Cliente cliente1 = new Cliente("111.111.111-11", "João da Silva", "Rua A, 123", "1990");
         Cliente cliente2 = new Cliente("222.222.222-22", "Maria Souza", "Avenida B, 456", "1985");
 
-        ContaCorrente conta = new ContaCorrente("12345-6", "???", cliente1, cliente2, 1000.0, 2000.0);
+        ContaCorrente conta = new ContaCorrente("4321421-6", "???", cliente1, cliente2, 1000.0, 2000.0);
 
         conta.creditar(500.0);
         conta.debitar(200.0);
